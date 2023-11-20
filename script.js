58717962a17f9f52461d7b62ec59982c38ad5f22
Tanu Chouksey <109014772+Tanuchouksey@users.noreply.github.com>
2023-11-20 17:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function firstChar(text) {
   // your code here
+  // Trim the input string to remove leading and trailing spaces
+  const trimmedString = inputString.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedString === '') {
+    return '';
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedString[0];
 }
+
+
 
 // Do not change the code below
 
